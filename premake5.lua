776053533364c1c02ec5af0e38d5699708f419c8
Lucho Suaya <luchosuaya@hotmail.com>
2021-07-15 20:20:48 +0200

    Added options for 'Dist' build config in premake file

--- premake5.lua
+++ premake5.lua
@@ -32,8 +32,10 @@ project "ImGui"
 
 	filter "configurations:Debug"
 		runtime "Debug"
-		symbols "on"
-
+		symbols "On"
 	filter "configurations:Release"
 		runtime "Release"
-		optimize "on"
+		optimize "On"
+	filter "configurations:Dist"
+        runtime "Release"
+        optimize "On"
